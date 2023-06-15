cd188b8f3d8ce3612cb4d7cc058da7c1b60273cb
Min seo <98958229+ms9876@users.noreply.github.com>
2023-06-15 16:24:39 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 128 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,40 +1,34 @@
 #include <iostream>
-#include <cstdlib>
-#include <map>
 #include <string>
+#include <map>
+#include <unordered_map>
 using namespace std;
 
-int main(void)
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL);
+int n, m;
+unordered_map<int, string> poketNum;
+unordered_map<string, int> poketStr;
 
-	map<string, int> poketStr;
-	map<int, string> poketNum;
-	int N, M, num=1;
-	cin >> N >> M;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 0; i < N; i++)
-	{
-		string str;
-		cin >> str;
-		poketStr.insert(make_pair(str, num));
-		poketNum.insert(make_pair(num++, str));
-	}
+    cin >> n >> m;
+    string poketmon;
 
-	for (int i = 0; i < M; i++)
-	{
-		char question[21];
-		cin >> question;
-		if (question[0] < 'A')
-		{
-			int qNum = atoi(question);
-			cout << poketNum[qNum] << '\n';
-		}
-		else
-		{
-			cout << poketStr[question] << '\n';
-		}
-	}
-	return 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> poketmon;
+        poketNum[i] = poketmon;
+        poketStr[poketmon] = i;
+    }
+
+    for (int i = 1; i <= m; i++) {
+        cin >> poketmon;
+        if (isdigit(poketmon[0])) {
+            cout << poketNum[atoi(poketmon.c_str())] << "\n";
+        }
+        else {
+            cout << poketStr[poketmon] << "\n";
+        }
+    }
 }
