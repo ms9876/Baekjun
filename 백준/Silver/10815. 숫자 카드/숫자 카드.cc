b6ee82e772a701232d546f88550c117f08aa143f
Min seo <98958229+ms9876@users.noreply.github.com>
2023-06-19 11:36:19 +0900

    [Silver V] Title: 숫자 카드, Time: 368 ms, Memory: 23040 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    unordered_set<int> s;
+    int n, m, num;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        s.insert(num);
+    }
+
+    cin >> m;
+
+    for (int i = 0; i < m; i++) {
+        cin >> num;
+        if (s.find(num) != s.end())
+            cout << "1 ";
+        else
+            cout << "0 ";
+    }
+
+    return 0;
+}
