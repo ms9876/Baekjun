8360bd89ce6111626386d0fa275ca2637ac451e6
Min seo <98958229+ms9876@users.noreply.github.com>
2023-07-27 09:47:34 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    
+	while (true)
+	{
+		cin >> a >> b;
+		if (a == 0 && b == 0) 
+            break;
+
+		if (a == 0 || b == 0)
+		{
+			cout << "neither" << "\n";
+		}
+		else
+		{
+			if (a % b == 0) 
+                cout << "multiple" << "\n";
+			else if (b % a == 0) 
+                cout << "factor" << "\n";
+			else 
+                cout << "neither" << "\n";
+		}
+	}
+}
