c762d3b9856ccfb2b41359170df4246731dac4a9
Min seo <98958229+ms9876@users.noreply.github.com>
2023-10-10 11:41:48 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -3,41 +3,43 @@
 #include <algorithm>
 using namespace std;
 
-int n, vn;
-int m, vm;
+int n, num_n;
+int m, num_m;
 vector<int> v;
 
-int bisearch(int x) {
-    int low = 0;
-    int high = n - 1;
+int Binary(int x) {
+	int left = 0;
+	int right = n - 1;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (v[mid] == x) return 1;
-        else if (v[mid] > x) high = mid - 1;
-        else if (v[mid] < x) low = mid + 1;
-    }
-    return 0;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (v[mid] == x) return 1;
+		else if (v[mid] > x) right = mid - 1;
+		else if (v[mid] < x) left = mid + 1;
+	}
+	return 0;
 }
 
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> vn;
-        v.push_back(vn);
-    }
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num_n;
+		v.push_back(num_n);
+	}
 
-    sort(v.begin(), v.end());
+	sort(v.begin(), v.end());
 
-    cin >> m;
-    for (int i = 0; i < m; i++)
-    {
-        cin >> vm;
-        cout << bisearch(vm) << '\n';
-    }
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> num_m;
+		cout << Binary(num_m) << '\n';
+	}
 }
