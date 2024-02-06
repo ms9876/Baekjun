c4f5f4675f77983b00e137c4270c418071e95a3b
Min seo <98958229+ms9876@users.noreply.github.com>
2024-02-06 19:35:21 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    char c;
+    
+    cin >> c;
+    
+    if (c == 'N' || c == 'n') 
+        cout << "Naver D2";
+    else 
+        cout << "Naver Whale";
+}
