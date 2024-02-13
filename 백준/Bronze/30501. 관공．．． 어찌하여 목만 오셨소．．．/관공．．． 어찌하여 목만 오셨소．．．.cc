6653fcc5f66ad2e28718c230884afeea83ef0b54
Min seo <98958229+ms9876@users.noreply.github.com>
2024-02-13 15:54:07 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    string input;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input;
+        if (input.find("S") != string::npos)
+        {
+            cout << input;
+        }
+    }
+}
