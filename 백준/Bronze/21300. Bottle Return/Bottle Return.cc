4f1dbc6fbfb41b7881c571ef1cff58c5f186f54e
Min seo <98958229+ms9876@users.noreply.github.com>
2024-03-09 22:45:49 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int sum = 0;
+    for (int i = 0; i < 6; i++) {
+        cin >> n;
+        sum += n;
+    }
+    cout << sum * 5;
+}
