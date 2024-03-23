d8113d7c39157afc25d462acda1551d38d721254
Min seo <98958229+ms9876@users.noreply.github.com>
2024-03-23 20:57:49 +0900

    [Bronze IV] Title: 주사위 세개, Time: 60 ms, Memory: 5496 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cs
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cs
@@ -6,9 +6,10 @@ if (a == b && b == c)
     result = 10000 + a * 1000;
 else if (a == b || a == c)
     result = 1000 + a * 100;
-else if (b == c)
-    result = 1000 + b * 100;
 else if(a !=b && b != c && a != c)
     result = Math.Max(a, Math.Max(b, c)) * 100;
+else if (b == c)
+    result = 1000 + b * 100;
+
     
 Console.WriteLine(result);
