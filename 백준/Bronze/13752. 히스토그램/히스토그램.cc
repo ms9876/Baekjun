4b1b8e77902b77ba0a5a78575e197f84a1ee9e5e
Min seo <98958229+ms9876@users.noreply.github.com>
2024-04-14 22:57:55 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n;
+        for (int j = 0; j < n; j++)
+        {
+            cout << "=";
+        }
+        cout << endl;
+    }
+}
