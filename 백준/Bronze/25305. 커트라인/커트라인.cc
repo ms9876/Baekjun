c1b07ea0ef1d4c2717379fa538cac0ecff63a360
Min seo <98958229+ms9876@users.noreply.github.com>
2024-04-30 18:46:21 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    int arr[1001];
+
+    cin >> n >> k;
+    
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+    
+    sort(arr, arr + n);
+    
+    cout << arr[n - k];
+}
