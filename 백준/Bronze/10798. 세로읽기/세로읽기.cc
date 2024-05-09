5c3b044ce8751ce164d4c92becd1728a39f9c626
Min seo <98958229+ms9876@users.noreply.github.com>
2024-05-09 14:10:07 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string arr[5];
+    
+    for(int i = 0; i < 5; ++i) 
+        cin >> arr[i];
+    for (int i = 0; i < 15; ++i) {
+        for (int j = 0; j < 5; ++j) 
+            if (arr[j].length() > i) 
+                cout << arr[j][i];
+    }
+}
