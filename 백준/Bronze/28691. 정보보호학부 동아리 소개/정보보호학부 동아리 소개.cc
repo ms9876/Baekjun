79520aa008f0fde406e588407a961aeb21327cdb
Min seo <98958229+ms9876@users.noreply.github.com>
2024-07-27 21:16:25 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char n;
+    cin >> n;
+    switch (n)
+    {
+    case 'M':
+        cout << "MatKor";
+        break;
+    case 'W':
+        cout << "WiCys";
+        break;
+    case 'C':
+        cout << "CyKor";
+        break;
+    case 'A':
+        cout << "AlKor";
+        break;
+    case '$':
+        cout << "$clear";
+        break;
+    }
+}
