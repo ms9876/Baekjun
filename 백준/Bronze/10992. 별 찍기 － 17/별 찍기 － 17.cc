7ffff5827a2a559245567e1378e39ad6cf0ccb3c
Min seo <98958229+ms9876@users.noreply.github.com>
2024-08-29 19:45:32 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - i; j++)
+        {
+            cout << ' ';
+        }
+        cout << '*';
+        if (i == 0) 
+        {
+            cout << '\n';
+            continue;
+        }
+        for (int j = 0; j < i * 2 - 1; j++)
+        {
+            if (i == n - 1)
+            {
+                cout << '*';
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << '*' << '\n';
+    }
+}
