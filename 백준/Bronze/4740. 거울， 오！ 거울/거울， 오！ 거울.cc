fbb46a55bf5a4639d227de2bcda4f6601c32fe3e
Min seo <98958229+ms9876@users.noreply.github.com>
2024-10-18 21:48:34 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string s;
+    
+    while(true) {
+        getline(cin, s);
+        
+        if(s == "***") 
+            break;
+        
+        reverse(s.begin(), s.end()); 
+        cout << s << endl;
+    }
+}
